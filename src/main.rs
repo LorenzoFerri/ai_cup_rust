a6a3049d1e25c66a5603188b4379b1c8230b55e3
Lorenzo Ferri <lorenzo.ferri@usi.ch>
2021-04-19 20:30:34 +0200

    Reading args

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
 mod point;
 mod problem;
 mod solution;
-
 use problem::Problem;
 use solution::Solution;
+use std::env;
 
 fn main() {
-    let problem = Problem::from_file("./problems/fl1577.tsp");
+    let args: Vec<String> = env::args().collect();
+    let default = String::from("./problems/ch130.tsp");
+    let filename = args.get(1).unwrap_or(&default);
+    let problem = Problem::from_file(filename);
     match problem {
         Ok(problem) => {
             let solution = Solution::new(&problem);
